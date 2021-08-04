f51784436dc420408537be85ce82649613077663
jousethen <jthen93@gmail.com>
2021-08-04 13:51:16 -0400

    LEsson complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+
+let vElement = document.createElement('h1');
+vElement.id = "victory";
+vElement.innerHTML = "Jouse is the champion";
+
+document.body.appendChild(vElement);
+let newHeader = document.getElementById("victory");
